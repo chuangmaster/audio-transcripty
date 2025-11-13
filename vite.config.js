cad6acd4077dc1224a339d38cc487491d99b981a
Zander <chuang.master@gmail.com>
2025-11-13 09:27:53 +0800

    chore: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  // GitHub Pages 部署配置
+  // 倉庫名稱為 audio-transcripty，所以 base 路徑需要設置為 '/audio-transcripty/'
+  base: '/audio-transcripty/',
+  
   plugins: [vue()],
+  
   server: {
-    port: 3000
+    port: 3000,
+    open: true
   }
 })
